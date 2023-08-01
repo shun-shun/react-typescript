03c6bc76bd85e2e73fb3815420c12b578a367879
shun-shun <gondo.syun0711@gmail.com>
2023-08-01 12:34:51 +0900

    com

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { Todo } from "./Todo";
+import { TodoType } from "./types/todo";
 
 function App() {
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
+
+  const onClickFetchData = () => {
+    axios
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        setTodos(res.data);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={onClickFetchData}>データ取得</button>
+      {todos.map((todo) => (
+        <Todo
+          key={todo.id}
+          title={todo.title}
+          userId={todo.userId}
+          completed={todo.completed}
+        />
+      ))}
     </div>
   );
 }
---
+++ src/Todo.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import { TodoType } from "./types/todo";
+
+export const Todo: FC<Omit<TodoType, "id">> = (props) => {
+  const { title, userId, completed } = props;
+  const completedMark = completed ? "[完]" : "[未]";
+  return (
+    <p>
+      `{completedMark} {title}(ユーザ:{userId})`
+    </p>
+  );
+};
